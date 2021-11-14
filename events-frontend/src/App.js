469ba8d36a1f1f8803c4d3936b9cdc0eebb79073
john katua <johnkatua99@gmail.com>
2021-11-15 00:22:31 +0300

    created two folders for our new project

---
+++ events-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './components/Login';
+import Registration from './components/Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
---
+++ events-frontend/src/components/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Registration = () => {
+  return (
+    <div>
+      Registration component
+    </div>
+  )
+}
+
+export default Registration
