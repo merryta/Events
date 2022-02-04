80dff0886f7de9417b0a74a4f6eadb3b363047c6
john katua <johnkatua99@gmail.com>
2022-02-04 03:11:51 +0300

    Deleted the util folder because it is no longer in use

--- events-backend/src/middleware/multer.js
+++
@@ -1,9 +0,0 @@
-const multer = require("multer");
-const Datauri = require("datauri");
-const path = require("path");
-
-const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).single("photo");
-
-
-module.exports = { multerUploads };
